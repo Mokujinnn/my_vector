55ee808c38eef16e843663a5a018eafdf82c7689
Lyubitskiy Maxim <freshentree99@gmail.com>
2024-05-04 16:28:33 +0700

    Add capacity methods

--- myvector.hpp
+++ myvector.hpp
@@ -8,60 +8,73 @@ namespace my
     class vector
     {
     private:
-        size_t capacity;
-        size_t size;
-        T *data;
+        size_t capacity_;
+        size_t size_;
+        T *data_;
 
     public:
+        // Constructors and destructor
         vector();
         vector(size_t _size);
         vector(size_t _size, const T &value);
         vector(const vector &vec);
         const vector &operator=(const vector &vec);
         ~vector();
+
+        // class iterator;
+        // iterator begin();
+        // const iterator cbegin();
+        // iterator end();
+
+        // Capacity
+        size_t size() const noexcept;
+        size_t capacity() const noexcept;
+        constexpr bool empty() const noexcept;
+        void reserve(size_t n);
+        void shrink_to_fit();
     };
 
     template <class T>
     inline vector<T>::vector()
-        : capacity(0),
-          size(0),
-          data(nullptr)
+        : capacity_(0),
+          size_(0),
+          data_(nullptr)
     {
     }
 
     template <class T>
     inline vector<T>::vector(size_t _size)
-        : capacity(_size),
-          size(_size),
-          data(new T[_size])
+        : capacity_(_size),
+          size_(_size),
+          data_(new T[_size])
     {
         for (size_t i = 0; i < _size; ++i)
         {
-            data[i] = T();
+            data_[i] = T();
         }
     }
 
     template <class T>
     inline vector<T>::vector(size_t _size, const T &value)
-        : capacity(_size),
-          size(_size),
-          data(new T[_size])
+        : capacity_(_size),
+          size_(_size),
+          data_(new T[_size])
     {
         for (size_t i = 0; i < _size; ++i)
         {
-            data[i] = value;
+            data_[i] = value;
         }
     }
 
     template <class T>
     inline vector<T>::vector(const vector<T> &vec)
-        : capacity(vec.capacity),
-          size(vec.size),
-          data(new T[vec.capacity])
+        : capacity_(vec.capacity_),
+          size_(vec.size_),
+          data_(new T[vec.capacity_])
     {
-        for (size_t i = 0; i < vec.size; ++i)
+        for (size_t i = 0; i < vec.size_; ++i)
         {
-            data[i] = vec.data[i];
+            data_[i] = vec.data_[i];
         }
     }
 
@@ -72,24 +85,71 @@ namespace my
         {
             return *this;
         }
-        
-        delete [] data;
-        data = new T[vec.capacity];
-        capacity = vec.capacity;
-        size = vec.size;
 
-        for (size_t i = 0; i < vec.capacity; ++i)
+        delete[] data_;
+        data_ = new T[vec.capacity_];
+        capacity_ = vec.capacity_;
+        size_ = vec.size_;
+
+        for (size_t i = 0; i < vec.capacity_; ++i)
         {
-            data[i] = vec.data[i];
+            data_[i] = vec.data_[i];
         }
-        
+
         return *this;
     }
 
     template <class T>
     vector<T>::~vector()
     {
-        delete[] data;
+        delete[] data_;
+    }
+
+    template <class T>
+    inline size_t vector<T>::size() const noexcept
+    {
+        return size_;
+    }
+
+    template <class T>
+    inline size_t vector<T>::capacity() const noexcept
+    {
+        return capacity_;
+    }
+
+    template <class T>
+    inline constexpr bool vector<T>::empty() const noexcept
+    {
+        return size_ == 0;
+    }
+
+    template <class T>
+    inline void vector<T>::reserve(size_t n)
+    {
+        if (n > capacity_)
+        {
+            T *a = new T[n];
+            for (size_t i = 0; i < size_; i++)
+            {
+                a[i] = data_[i];
+            }
+            delete[] data_;
+            data_ = a;
+            capacity_ = n;
+        }
+    }
+
+    template <class T>
+    inline void vector<T>::shrink_to_fit()
+    {
+        T *a = new T[size_];
+        for (size_t i = 0; i < size_; i++)
+        {
+            a[i] = data_[i];
+        }
+        delete[] data_;
+        data_ = a;
+        capacity_ = size_;
     }
 
 } // namespace my
